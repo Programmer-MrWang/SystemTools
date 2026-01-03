09a525ee4fed9e5a5bab1909927af69dc5a57690
Wang Haoyu <WangHaoyu-geren@outlook.com>
2026-01-03 23:54:10 +0800

    feat: 新增“文件操作”

---
+++ Settings/MoveSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SystemTools.Settings;
+
+public class MoveSettings
+{
+    [JsonPropertyName("operationType")]
+    public string OperationType { get; set; } = "文件";
+
+    [JsonPropertyName("sourcePath")]
+    public string SourcePath { get; set; } = string.Empty;
+
+    [JsonPropertyName("destinationPath")]
+    public string DestinationPath { get; set; } = string.Empty;
+}

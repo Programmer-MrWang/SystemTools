d6cda924ae6dbf47742fb6aa84258ad9621de7f9
Wang Haoyu <WangHaoyu-geren@outlook.com>
2026-02-13 21:07:29 +0800

    feat: 添加配置存储功能

---
+++ MainConfigData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace SystemTools.ConfigHandlers;
+
+public class MainConfigData : INotifyPropertyChanged
+{
+    public event EventHandler? RestartPropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    bool _enableExperimentalFeatures;
+
+    [JsonPropertyName("enableExperimentalFeatures")]
+    public bool EnableExperimentalFeatures
+    {
+        get => _enableExperimentalFeatures;
+        set
+        {
+            if (value == _enableExperimentalFeatures) return;
+            _enableExperimentalFeatures = value;
+            OnPropertyChanged();
+            RestartPropertyChanged?.Invoke(this, EventArgs.Empty); 
+        }
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Shared/InjectServices.cs
@@ -0,0 +1,39 @@
+﻿using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Extensions.Registry;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace SystemTools.Shared;
+
+public static class InjectServices
+{
+    public static bool TryGetAddSettingsPageGroupMethod([MaybeNullWhen(false)] out MethodInfo method)
+    {
+        var settingsWindowRegistryExtensionsType = typeof(SettingsWindowRegistryExtensions);
+        method = settingsWindowRegistryExtensionsType
+            .GetMethods()
+            .FirstOrDefault(m => (m.ToString()?.Contains("AddSettingsPageGroup") ?? false)
+                                 && m.GetParameters().Length == 4);
+        return method != null;
+    }
+
+    public static FieldInfo GetSettingsPageInfoNameField()
+    {
+        var settingsPageInfoType = typeof(SettingsPageInfo);
+        var field = settingsPageInfoType
+            .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+            .FirstOrDefault(f => f.ToString()?.Contains("Name") ?? false);
+        return field!;
+    }
+
+    public static PropertyInfo GetSettingsPageInfoGroupIdProperty()
+    {
+        var settingsPageInfoType = typeof(SettingsPageInfo);
+        var property = settingsPageInfoType
+            .GetProperties()
+            .FirstOrDefault(p => p.ToString()?.Contains("GroupId") ?? false);
+        return property!;
+    }
+}

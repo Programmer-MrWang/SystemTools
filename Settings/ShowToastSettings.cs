81d25940c3bafef83e2ebd92ed165dcf8afa1d01
Wang Haoyu <WangHaoyu-geren@outlook.com>
2026-02-13 21:06:51 +0800

    feat: 新增“拉起自定义Windows通知”行动

---
+++ Settings/ShowToastSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SystemTools.Settings;
+
+public class ShowToastSettings
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("content")]
+    public string Content { get; set; } = string.Empty;
+}

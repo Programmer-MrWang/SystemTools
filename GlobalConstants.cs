05051a1b09f7553e17439a0e66fc48c285942af1
Wang Haoyu <WangHaoyu-geren@outlook.com>
2026-02-13 21:08:39 +0800

    feat: 方法引用

---
+++ GlobalConstants.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using SystemTools.ConfigHandlers;
+using Microsoft.Extensions.Logging.Console;
+using System.Reflection;
+
+namespace SystemTools.Shared;
+
+public static class GlobalConstants
+{
+    public static string? PluginConfigFolder { get; set; }
+
+    public static ConfigHandlers.MainConfigHandler? MainConfig { get; set; }
+
+    public static class HostInterfaces
+    {
+        public static ILogger<Plugin>? PluginLogger;
+    }
+
+    public static class Information
+    {
+        public static string PluginFolder { get; set; } = string.Empty;
+        public static string PluginVersion { get; set; } = "???";
+    }
+
+    public static bool ShowChangelogOnOpen { get; set; } = false;
+}
+
+

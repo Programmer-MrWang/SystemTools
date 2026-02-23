74934c54997822ee32b338cd8ea9172a60657d7b
Wang Haoyu <WangHaoyu-geren@outlook.com>
2026-02-23 18:04:50 +0800

    feat: 新增“网络延迟检测”组件

---
+++ Models/ComponentSettings/NetworkStatusSettings.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SystemTools.Models.ComponentSettings;
+
+public partial class NetworkStatusSettings : ObservableObject
+{
+    [ObservableProperty]
+    private string _pingUrl = "https://baidu.com";
+
+    [ObservableProperty]
+    private string _displayText = "网络延迟 ";
+}

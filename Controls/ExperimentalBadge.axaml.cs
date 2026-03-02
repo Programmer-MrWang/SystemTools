f2fa4464b5a0ec4b3c24f782c99e7d929c1dde90
Programmer_MrWang <WangHaoyu-geren@outlook.com>
2026-03-02 08:32:00 +0800

    feat: 添加“实验性”标签

---
+++ Controls/ExperimentalBadge.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Controls;
+
+namespace SystemTools.Controls;
+
+public partial class ExperimentalBadge : UserControl {
+    public ExperimentalBadge() {
+        InitializeComponent();
+    }
+}

be2b75b3d536165ccaa9c3426406a66f16e9833d
diann34 <diannwork@outlook.com>
2026-06-14 06:10:23 +0800

    Revert "feat:添加语音播报行动"
    This reverts commit bc8fd30c86c1bbc7e4bf06742bd87690b6a14866.

--- Settings/DoSpeechSettings.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace SystemTools.Settings;
-
-public class DoSpeechSettings
-{
-    [JsonPropertyName("text")]
-    public string Text { get; set; } = string.Empty;
-}

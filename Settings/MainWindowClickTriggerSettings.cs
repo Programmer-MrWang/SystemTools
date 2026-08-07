60b4aa405fbe4b88668ae31e6ae921d32250f33f
Wang Haoyu <WangHaoyu-geren@outlook.com>
2026-08-07 23:05:03 +0800

    fix: 修正部分表述

--- Settings/MainWindowClickTriggerSettings.cs
+++ Settings/MainWindowClickTriggerSettings.cs
@@ -11,7 +11,7 @@ public sealed class MainWindowClickTriggerSettings : TriggerSettingsControlBase<
         Content = new TextBlock
         {
             Margin = new(10),
-            Text = "在ClassIsland主界面上进行左键单击操作时触发。",
+            Text = "在ClassIsland主界面上进行左键单击操作时触发。\n启用后仍可实现穿透点击 但同时会触发触发器",
             TextWrapping = TextWrapping.Wrap,
             Foreground = Brushes.Gray
         };
